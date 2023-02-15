edb135c93b49f97345b5733503d416248a35cdfc
saubby <saubby27@gmail.com>
2023-02-15 22:49:07 +0530

    Added manifest

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -29,6 +29,24 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just another text editor',
+        short_name: 'JATE',
+        description: 'Just another text editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
